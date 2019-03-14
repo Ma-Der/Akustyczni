1856908a9b6a1c647f4ed3c8ab466cdb3fb0695f
Maciej Derewiański <eco_derma@poczta.onet.pl>
2019-03-14 22:47:15 +0100

    Add AJAX for loading different pages

---
+++ js/script.js
@@ -0,0 +1,60 @@
+'use strict';
+
+function loadPage(href) {
+  return new Promise((resolve, reject) => {
+  const xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.open("GET", href);
+  xmlhttp.addEventListener('load', () => resolve(xmlhttp.responseText));
+  xmlhttp.addEventListener('error', () => reject(new Error(xmlhttp.statusText)));
+  xmlhttp.send();
+});
+}
+
+const mainPage = document.getElementById('main');
+
+document.addEventListener('DOMContentLoaded', function(){
+  loadPage('./src/start.html')
+    .then(
+      page => mainPage.innerHTML = page
+    );
+});
+
+const startPage = document.querySelector('.start');
+    startPage.addEventListener('click', function(){
+      loadPage('./src/start.html')
+        .then(
+          page => mainPage.innerHTML = page
+        );
+    });
+const bandPage = document.querySelector('.band');
+    bandPage.addEventListener('click', function(){
+      loadPage('./src/band.html')
+        .then(
+          page => mainPage.innerHTML = page
+        );
+    });
+
+const mediaPage = document.querySelector('.media');
+    mediaPage.addEventListener('click', function(){
+      loadPage('./src/media.html')
+        .then(
+          page => mainPage.innerHTML = page
+        );
+    });
+
+const offerPage = document.querySelector('.offer');
+    offerPage.addEventListener('click', function(){
+      loadPage('./src/offer.html')
+        .then(
+          page => mainPage.innerHTML = page
+        );
+    });
+
+    const contactPage = document.querySelector('.contact');
+        contactPage.addEventListener('click', function(){
+          loadPage('./src/contact.html')
+            .then(
+              page => mainPage.innerHTML = page
+            );
+        });
