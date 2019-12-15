0834c442bd2d15b61b8e7d8918f942e7d52290e2
Maciej Derewiański <eco_derma@poczta.onet.pl>
2019-12-15 21:36:46 +0100

    Add gallery carousel core

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
-
 function loadPage(href) {
   return new Promise((resolve, reject) => {
   const xmlhttp = new XMLHttpRequest();
@@ -60,8 +59,22 @@ const galleryPage = document.querySelector('.galeria');
     galleryPage.addEventListener('click', function(){
       loadPage('./src/galeria.html')
         .then(
-          page => mainPage.innerHTML = page
-        );
+          page => {
+            mainPage.innerHTML = page;
+            /* var elms = document.getElementsByClassName( 'splide' );
+            for ( var i = 0, len = elms.length; i < len; i++ ) {
+              new Splide( elms[ i ] ).mount();
+            } */
+            var elem = document.querySelector('.main-carousel');
+            var flkty = new Flickity( elem, {
+              // options
+              cellAlign: 'left',
+              contain: true,
+              groupCells: true,
+              wrapAround: true
+            });
+          }
+        )
     });
 
 const offerPage = document.querySelector('.faq');
@@ -76,7 +89,46 @@ const contactPage = document.querySelector('.contact');
     contactPage.addEventListener('click', function(){
       loadPage('./src/contact.html')
         .then(
-          page => mainPage.innerHTML = page
+          page => {
+            mainPage.innerHTML = page;
+            window.addEventListener("DOMContentLoaded", function() {
+
+              var form = document.getElementById("akustyczni-form");
+              var button = document.getElementById("form-button");
+              var status = document.getElementById("form-status");
+
+              function success() {
+                form.reset();
+                button.style = "display: none";
+                status.innerHTML = "Dziękujemy za wiadomość :)";
+              }
+
+              function error() {
+                status.innerHTML = "Coś poszło nie tak, spróbuj ponownie :(";
+              }
+
+              form.addEventListener("submit", function(ev) {
+                ev.preventDefault();
+                var data = new FormData(form);
+                ajax(form.method, form.action, data, success, error);
+              });
+            });
+
+            function ajax(method, url, data, success, error) {
+              var xhr = new XMLHttpRequest();
+              xhr.open(method, url);
+              xhr.setRequestHeader("Accept", "application/json");
+              xhr.onreadystatechange = function() {
+                if (xhr.readyState !== XMLHttpRequest.DONE) return;
+                if (xhr.status === 200) {
+                  success(xhr.response, xhr.responseType);
+                } else {
+                  error(xhr.status, xhr.response, xhr.responseType);
+                }
+              };
+              xhr.send(data);
+            }
+          }
         );
     });
 
